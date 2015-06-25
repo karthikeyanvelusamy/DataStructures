94c2b6ac5db7e10e847d45ae2ebe9d627348964d
Karthikeyan <karthikeyanvelusamycs@gmail.com>
2015-06-26 00:37:27 +0530

    pre order and in order update

--- DataStructuresTutorials/src/com/decoderz/ds/tree/BinaryTreeImpl.java
+++ DataStructuresTutorials/src/com/decoderz/ds/tree/BinaryTreeImpl.java
@@ -1,12 +1,14 @@
 package com.decoderz.ds.tree;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class BinaryTreeImpl {
 
+	
 	public static void main(String[] args) throws Exception {
-
+		// Binary Tree Construction with given inOrder and postOrder
 		BinaryTreeImpl binryTreeImp = new BinaryTreeImpl();
 
 		char[] inOrder = new char[] { 'D', 'B', 'E', 'A', 'F', 'C', 'G' };
@@ -23,16 +25,27 @@ public class BinaryTreeImpl {
 		for (char i : postOrder)
 			postOrderList.add(i);
 
+		// Connstructing Binary Tree by recursive calls
 		Node root = binryTreeImp.constructTree(inOrderList, postOrderList, 0,
-				inOrder.length-1, 0, postOrder.length-1);
+				inOrder.length - 1, 0, postOrder.length - 1);
 
+		
+		System.out.println("Pre Order With Recurssion : ");
 		binryTreeImp.preOrderWithRecursion(root);
 
+		System.out.println();
+		System.out.println("Pre Order With Out Recurssion : ");
+		binryTreeImp.preOrderWithOutRecurssion(root);
+		
+		System.out.println("In Order With Recurssion : ");
+		binryTreeImp.inOrderWithRecursion(root);
+
+		
 	}
 
 	private Node constructTree(List<Character> inOrderList,
-			List<Character> postOrderList, int inStart, int inEnd, int postStart,
-			int postEnd) throws Exception {
+			List<Character> postOrderList, int inStart, int inEnd,
+			int postStart, int postEnd) throws Exception {
 
 		if (inStart > inEnd || postStart > postEnd)
 			return null;
@@ -49,13 +62,62 @@ public class BinaryTreeImpl {
 		return root;
 	}
 
+	/**
+	 * Method for PreOrder Traversal for a binary tree with recursive calls.
+	 * 
+	 * @param root
+	 */
 	private void preOrderWithRecursion(Node root) {
 
 		if (root != null) {
-			System.out.print(root.getData()+"\t");
+			System.out.print(root.getData() + " ");
 			preOrderWithRecursion(root.getLeft());
 			preOrderWithRecursion(root.getRight());
 
 		}
+
 	}
+
+	/**
+	 * Method for pre order traversal for without using recursion.
+	 * 
+	 * @param root
+	 */
+	private void preOrderWithOutRecurssion(Node root) {
+		LinkedList<Node> stack = new LinkedList<Node>();
+
+		stack.push(root);
+		while (!stack.isEmpty()) {
+			Node temp = stack.pop();
+			System.out.print(temp.getData() + " ");
+
+			if (temp.getRight() != null)
+				stack.push(temp.getRight());
+
+			if (temp.getLeft() != null)
+				stack.push(temp.getLeft());
+
+		}
+		System.out.println();
+	}
+
+	
+
+	/**
+	 * Method for InOrder Traversal for a binary tree with recursive calls.
+	 * 
+	 * @param root
+	 */
+	private void inOrderWithRecursion(Node root) {
+
+		if (root != null) {
+			inOrderWithRecursion(root.getLeft());
+			System.out.print(root.getData() + " ");
+			inOrderWithRecursion(root.getRight());
+
+		}
+
+	}
+
+
 }
